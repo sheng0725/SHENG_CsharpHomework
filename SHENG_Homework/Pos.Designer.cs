fec626cdc85007df85ffde30059413a1e1a56c2b
sheng0725 <jame753951@gmail.com>
2023-06-15 17:42:29 +0800

    圖片修改

--- SHENG_Homework/Pos.Designer.cs
+++ SHENG_Homework/Pos.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pos));
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btncookie = new System.Windows.Forms.Button();
@@ -35,17 +36,17 @@
             this.btncake = new System.Windows.Forms.Button();
             this.btncoffee = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.labtotal = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.btncard = new System.Windows.Forms.Button();
             this.btncash = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.labList = new System.Windows.Forms.Label();
             this.btnListclear = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.labtotal = new System.Windows.Forms.Label();
-            this.labList = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -70,6 +71,7 @@
             this.panel1.Controls.Add(this.btncake);
             this.panel1.Controls.Add(this.btncoffee);
             this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.panel1.Location = new System.Drawing.Point(16, 63);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(230, 300);
@@ -77,41 +79,55 @@
             // 
             // btncookie
             // 
+            this.btncookie.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btncookie.BackgroundImage")));
+            this.btncookie.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btncookie.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btncookie.ImageAlign = System.Drawing.ContentAlignment.TopRight;
             this.btncookie.Location = new System.Drawing.Point(125, 170);
             this.btncookie.Name = "btncookie";
             this.btncookie.Size = new System.Drawing.Size(80, 100);
             this.btncookie.TabIndex = 4;
-            this.btncookie.Text = "餅乾";
+            this.btncookie.Text = "餅乾\r\n$80";
             this.btncookie.UseVisualStyleBackColor = true;
             this.btncookie.Click += new System.EventHandler(this.btncookie_Click);
             // 
             // btnbread
             // 
+            this.btnbread.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnbread.BackgroundImage")));
+            this.btnbread.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnbread.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btnbread.Location = new System.Drawing.Point(23, 170);
             this.btnbread.Name = "btnbread";
             this.btnbread.Size = new System.Drawing.Size(80, 100);
             this.btnbread.TabIndex = 3;
-            this.btnbread.Text = "麵包";
+            this.btnbread.Text = "麵包\r\n$150";
             this.btnbread.UseVisualStyleBackColor = true;
             this.btnbread.Click += new System.EventHandler(this.btnbread_Click);
             // 
             // btncake
             // 
+            this.btncake.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btncake.BackgroundImage")));
+            this.btncake.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btncake.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btncake.Location = new System.Drawing.Point(125, 51);
             this.btncake.Name = "btncake";
             this.btncake.Size = new System.Drawing.Size(80, 100);
             this.btncake.TabIndex = 2;
-            this.btncake.Text = "蛋糕";
+            this.btncake.Text = "蛋糕\r\n$200";
             this.btncake.UseVisualStyleBackColor = true;
             this.btncake.Click += new System.EventHandler(this.btncake_Click);
             // 
             // btncoffee
             // 
+            this.btncoffee.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btncoffee.BackgroundImage")));
+            this.btncoffee.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btncoffee.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btncoffee.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btncoffee.Location = new System.Drawing.Point(23, 51);
             this.btncoffee.Name = "btncoffee";
             this.btncoffee.Size = new System.Drawing.Size(80, 100);
             this.btncoffee.TabIndex = 1;
-            this.btncoffee.Text = "咖啡";
+            this.btncoffee.Text = "咖啡\r\n$100\r\n";
             this.btncoffee.UseVisualStyleBackColor = true;
             this.btncoffee.Click += new System.EventHandler(this.btncoffee_Click);
             // 
@@ -120,10 +136,23 @@
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.labtotal);
             this.panel2.Controls.Add(this.label2);
+            this.panel2.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.panel2.Location = new System.Drawing.Point(252, 63);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(250, 100);
             this.panel2.TabIndex = 3;
+            // 
+            // labtotal
+            // 
+            this.labtotal.BackColor = System.Drawing.Color.Black;
+            this.labtotal.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labtotal.ForeColor = System.Drawing.Color.White;
+            this.labtotal.Location = new System.Drawing.Point(15, 39);
+            this.labtotal.Name = "labtotal";
+            this.labtotal.Size = new System.Drawing.Size(218, 40);
+            this.labtotal.TabIndex = 6;
+            this.labtotal.Text = "NT$ 0";
+            this.labtotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label2
             // 
@@ -141,6 +170,7 @@
             this.panel3.Controls.Add(this.btncard);
             this.panel3.Controls.Add(this.btncash);
             this.panel3.Controls.Add(this.label4);
+            this.panel3.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.panel3.Location = new System.Drawing.Point(252, 170);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(250, 100);
@@ -184,10 +214,23 @@
             this.panel4.Controls.Add(this.labList);
             this.panel4.Controls.Add(this.btnListclear);
             this.panel4.Controls.Add(this.label6);
+            this.panel4.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.panel4.Location = new System.Drawing.Point(508, 64);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(280, 300);
             this.panel4.TabIndex = 5;
+            // 
+            // labList
+            // 
+            this.labList.BackColor = System.Drawing.Color.White;
+            this.labList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labList.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labList.Location = new System.Drawing.Point(14, 34);
+            this.labList.Name = "labList";
+            this.labList.Size = new System.Drawing.Size(254, 218);
+            this.labList.TabIndex = 6;
+            this.labList.Text = "尚未點餐";
+            this.labList.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // btnListclear
             // 
@@ -214,36 +257,12 @@
             // 
             this.label5.AutoSize = true;
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label5.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label5.Location = new System.Drawing.Point(399, 243);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(105, 22);
+            this.label5.Size = new System.Drawing.Size(80, 18);
             this.label5.TabIndex = 5;
             this.label5.Text = "信用卡享9折!";
-            // 
-            // labtotal
-            // 
-            this.labtotal.BackColor = System.Drawing.Color.Black;
-            this.labtotal.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.labtotal.ForeColor = System.Drawing.Color.White;
-            this.labtotal.Location = new System.Drawing.Point(15, 39);
-            this.labtotal.Name = "labtotal";
-            this.labtotal.Size = new System.Drawing.Size(218, 40);
-            this.labtotal.TabIndex = 6;
-            this.labtotal.Text = "NT$ 0";
-            this.labtotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // labList
-            // 
-            this.labList.BackColor = System.Drawing.Color.White;
-            this.labList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.labList.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.labList.Location = new System.Drawing.Point(14, 34);
-            this.labList.Name = "labList";
-            this.labList.Size = new System.Drawing.Size(254, 218);
-            this.labList.TabIndex = 6;
-            this.labList.Text = "尚未點餐";
-            this.labList.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // Pos
             // 
