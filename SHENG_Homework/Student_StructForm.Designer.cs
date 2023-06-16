d953b2ba9a4cafe0235d7a46bcedf6eae8c8890b
sheng0725 <jame753951@gmail.com>
2023-06-16 13:13:11 +0800

    0

--- SHENG_Homework/Student_StructForm.Designer.cs
+++ SHENG_Homework/Student_StructForm.Designer.cs
@@ -196,6 +196,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.butmaxmin);
             this.Controls.Add(this.groupBox1);
